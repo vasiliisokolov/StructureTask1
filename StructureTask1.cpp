25589bd827a6dd393c7fb986937025b569a79759
VasiliiSokolov <vasilysokolov@mail.ru>
2022-12-21 22:19:28 +0300

    Some problem with read

--- StructureTask1.cpp
+++ StructureTask1.cpp
@@ -10,6 +10,7 @@ struct staff
     int sum = 0;
 };
 
+void init_staf(staff&, std::string&);
 void data_write(staff&);
 void data_read();
 
@@ -19,20 +20,20 @@ int main()
     std::cout << "Choose the operation: " << std::endl;
     std::cout << "Press 1 to load data, press 2 to read the statement." << std::endl;
     std::cin >> control;
+    std::cin.ignore();
     if (control != 2 && control != 1)
     {
         std::cerr << "Error! Wrong command!" << std::endl;
     }
     else if (control == 1)
     {
-        std::cin.ignore();
         std::string buffer;
-        staff load;
         std::cout << "Enter employer's name, date and summ of transaction: " << std::endl;
         std::getline(std::cin, buffer);
-        std::stringstream temp_stream(buffer);
-        temp_stream >> load.name >> load.paymentDate >> load.sum;
-        std::cout << load.name;
+        staff load;
+        //std::cout << load.name << " " << load.paymentDate << " " << load.sum << std::endl;
+        init_staf(load, buffer);
+        //std::cout << load.name << " " << load.paymentDate << " " << load.sum << std::endl;
         data_write(load);
     }
     else if (control == 2)
@@ -41,19 +42,27 @@ int main()
     }
 }
 
+void init_staf(staff& load, std::string& buffer)
+{
+    
+    std::stringstream temp_stream(buffer);
+    temp_stream >> load.name >> load.paymentDate >> load.sum;
+}
+
 void data_write(staff& load)
 {
-    std::ofstream out("statemen.bin", std::ios::binary | std::ios::app);
+    std::ofstream out("statement.bin", std::ios::binary | std::ios::app);
     if (out.is_open())
     {
+        std::cout << load.name << " " << load.paymentDate << " " << load.sum << std::endl;
         int len = load.name.length();
-        out.write((char*)len, sizeof(len));
-        out.write(load.name.c_str(), sizeof(len));
+        out.write((char*)&len, sizeof(len));
+        out.write(load.name.c_str(), len);
         len = load.paymentDate.length();
-        out.write((char*)len, sizeof(len));
-        out.write(load.paymentDate.c_str(), sizeof(len));
-        out.write((char*)load.sum, sizeof(load.sum));
-        out << std::endl;
+        out.write((char*)&len, sizeof(len));
+        out.write(load.paymentDate.c_str(), len);
+        out.write((char*)&load.sum, sizeof(load.sum));
+        
         out.close();
     }
     else
@@ -64,5 +73,31 @@ void data_write(staff& load)
 
 void data_read()
 {
-    std::ifstream in("statement.bin", std::ios::binary | std::ios::in)
+    
+    std::ifstream in("statement.bin", std::ios::binary);
+    if (in.is_open())
+    {
+        staff show;
+        while (!in.eof())
+        {
+            
+            std::cout << show.name << " " << show.paymentDate << " " << show.sum << std::endl;
+            int len;
+            in.read((char*)&len, sizeof(len));
+            show.name.resize(len);
+            in.read((char*)show.name.c_str(), len);
+            in.read((char*)&len, sizeof(len));
+            show.paymentDate.resize(len);
+            in.read((char*)show.paymentDate.c_str(), len);
+            in.read((char*)&show.sum, sizeof(show.sum));
+            std::cout << show.name << " " << show.paymentDate << " " << show.sum << std::endl;
+
+        }
+        in.close();
+    }
+    else
+    {
+        std::cerr << "Error! Statement not found!" << std::endl;
+    }
+    
 }
