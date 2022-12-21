82ec0a27fedd0da929804ee6b92b666b6062d1b1
VasiliiSokolov <vasilysokolov@mail.ru>
2022-12-21 19:46:13 +0300

    load

--- StructureTask1.cpp
+++ StructureTask1.cpp
@@ -5,11 +5,12 @@
 
 struct staff
 {
-    std::string name;
-    std::string paymentDate;
+    std::string name = "unknown";
+    std::string paymentDate = "12.12.12";
     int sum = 0;
 };
-void data_write();
+
+void data_write(staff&);
 void data_read();
 
 int main()
@@ -32,17 +33,36 @@ int main()
         std::stringstream temp_stream(buffer);
         temp_stream >> load.name >> load.paymentDate >> load.sum;
         std::cout << load.name;
+        data_write(load);
     }
     else if (control == 2)
     {
-
+        data_read();
     }
 }
 
-void data_write()
+void data_write(staff& load)
 {
+    std::ofstream out("statemen.bin", std::ios::binary | std::ios::app);
+    if (out.is_open())
+    {
+        int len = load.name.length();
+        out.write((char*)len, sizeof(len));
+        out.write(load.name.c_str(), sizeof(len));
+        len = load.paymentDate.length();
+        out.write((char*)len, sizeof(len));
+        out.write(load.paymentDate.c_str(), sizeof(len));
+        out.write((char*)load.sum, sizeof(load.sum));
+        out << std::endl;
+        out.close();
+    }
+    else
+    {
+        std::cerr << "Error! Library file does not exist!" << std::endl;
+    }
 }
 
 void data_read()
 {
+    std::ifstream in("statement.bin", std::ios::binary | std::ios::in)
 }
